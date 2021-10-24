a7570421c99ec4a00c676c7b09278c1566164ee8
patiphan2000 <patiphan.sri@ku.th>
2021-10-24 18:45:47 +0700

    add test id and description

--- __test__/test_api.spec.js
+++ __test__/test_api.spec.js
@@ -2,17 +2,20 @@ const supertest = require('supertest')
 const request = supertest.agent('https://suchonsite-server.herokuapp.com');
 
 describe('People information', () => {
+  /**
+	 * Test ID: 1
+	 *
+	 * GET all people information.
+	 */
   it('all people information', () => {
     return request.get('/people/all').expect(200);
   });
 
-  it('no date', () => {
-    return request.get('/people/by_date/')
-    .then((response) => {
-      expect(response.status).toBe(202)
-    })
-  });
-
+  /**
+	 * Test ID: 2
+	 *
+	 * GET people information by specific date.
+	 */
   it('information from specific date', () => {
     return request.get('/people/by_date/20-10-2021')
     .then((response) => {
@@ -20,6 +23,23 @@ describe('People information', () => {
     })
   });
 
+  /**
+	 * Test ID: 3
+	 *
+	 * GET people information with no date given.
+	 */
+  it('no date', () => {
+    return request.get('/people/by_date/')
+    .then((response) => {
+      expect(response.status).toBe(202)
+    })
+  });
+
+  /**
+	 * Test ID: 4
+	 *
+	 * GET people information from the past that data don't exist.
+	 */
   it('information from the middle era', () => {
     return request.get('/people/by_date/20-10-1469')
     .then((response) => {
@@ -27,6 +47,11 @@ describe('People information', () => {
     })
   });
 
+  /**
+	 * Test ID: 5
+	 *
+	 * GET people information from the future that data don't exist.
+	 */
   it('information from the future', () => {
     return request.get('/people/by_date/20-10-2023')
     .then((response) => {
@@ -34,6 +59,11 @@ describe('People information', () => {
     })
   });
 
+  /**
+	 * Test ID: 6
+	 *
+	 * GET people information by using invalid date format.
+	 */
   it('invalid date format 1', () => {
     return request.get('/people/by_date/2021-10-20')
     .then((response) => {
@@ -41,6 +71,11 @@ describe('People information', () => {
     })
   });
 
+  /**
+	 * Test ID: 7
+	 *
+	 * GET people information by using invalid date format.
+	 */
   it('invalid date format 2', () => {
     return request.get('/people/by_date/20/OCT/2023')
     .then((response) => {
@@ -48,6 +83,11 @@ describe('People information', () => {
     })
   });
 
+  /**
+	 * Test ID: 8
+	 *
+	 * GET people information by using invalid date format.
+	 */
   it('invalid date format 3', () => {
     return request.get('/people/by_date/20-OCT-2023')
     .then((response) => {
@@ -58,14 +98,11 @@ describe('People information', () => {
 
 
 describe('Retrive data from Gov', () => {
-  it('no date', () => {
-    return request.post('/getDataFromGov/')
-    .then((response) => {
-      expect(response.status).toBe(202)
-      expect(response.body.msg).toBe('no date included.')
-    })
-  });
-
+  /**
+	 * Test ID: 9
+	 *
+	 * GET people information from Gov on specific date.
+	 */
   it('retrive data from specific date', () => {
     return request.post('/getDataFromGov/20-10-2021')
     .then((response) => {
@@ -74,6 +111,24 @@ describe('Retrive data from Gov', () => {
     })
   });
 
+  /**
+	 * Test ID: 10
+	 *
+	 * GET people information from Gov with no date given.
+	 */
+  it('no date', () => {
+    return request.post('/getDataFromGov/')
+    .then((response) => {
+      expect(response.status).toBe(202)
+      expect(response.body.msg).toBe('no date included.')
+    })
+  });
+
+  /**
+	 * Test ID: 11
+	 *
+	 * GET people information from Gov with invalid date format.
+	 */
   it('invalid date format 1', () => {
     return request.post('/getDataFromGov/2021-10-20')
     .then((response) => {
@@ -81,6 +136,11 @@ describe('Retrive data from Gov', () => {
     })
   });
 
+  /**
+	 * Test ID: 12
+	 *
+	 * GET people information from Gov with invalid date format.
+	 */
   it('invalid date format 2', () => {
     return request.post('/getDataFromGov/20/10/2021')
     .then((response) => {
@@ -88,6 +148,11 @@ describe('Retrive data from Gov', () => {
     })
   });
 
+  /**
+	 * Test ID: 13
+	 *
+	 * GET people information from Gov with invalid date format.
+	 */
   it('invalid date format 3', () => {
     return request.post('/getDataFromGov/20-OCT-2021')
     .then((response) => {
